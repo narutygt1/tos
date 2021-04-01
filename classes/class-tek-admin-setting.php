d6a649c10f03f365dddb43a8b7d547baadf2929b
narutygt1 <47191249+narutygt1@users.noreply.github.com>
2021-04-02 02:41:01 +0700

    Created structure functions to the admin setting

--- classes/class-tek-admin-setting.php
+++ classes/class-tek-admin-setting.php
@@ -1,55 +1,217 @@
 <?php
 
-class TEK_Admin_Setting 
+class TEK_Admin_Setting
 {
+	/**
+	 * Option Name.
+	 * @var string
+	 */
 	protected $option_name;
-	private $setting_name;
 
-	private $default_options = array(
-		"use_header_service_method"	=>	true
-	);
+	/**
+	 * Option Group Name.
+	 * @var string
+	 */
+	protected $option_group;
+
+	/**
+	 * Variables Setting.
+	 * @var array
+	 */
+	protected $register_setting_vars;
 
 	function __construct($option_name){
 		$this->option_name = $option_name;
+		$this->option_group = $option_name."_option_group";
 
 		if(empty($option_name))
-			throw new Exception('Error unset to {register_activation_hook: Name} of the option to retrieve');
+			throw new Exception('Error unset to register_activation_hook: {Name} of the option to retrieve');
 			
         // Register function to be called when the plugin is activated
-		register_activation_hook( TOS_PLUGIN_PATH, array( $this, 'set_default_options') );
+		register_activation_hook( TOS_PLUGIN_PATH, function(){
+			$this->set_option(array());
+		} );
     }
 
-    // Function called upon plugin activation to initialize the options values
-	// if they are not present already
-	function set_default_options() {
-		$this->set_options(array());
-	}
+	#region public
 
-	// Function to retrieve options from database as well as create or 
-	// add new options
-	public function set_options($args = array()) {
-		$options = get_option( $this->setting_api_name , array() );
-		$new_options = array();
+	public function set_option($args = array()) {
+		$options = get_option( $this->option_name , array() );
 
-		if(isset($options)){
-			$new_options = $this->default_options;
-		}
+	    $merged_option = wp_parse_args( $args, $options ); 
 
-	    $merged_options = wp_parse_args( $options, $new_options ); 
-
-	    $compare_options = array_diff_key( $new_options, $options );   
+	    $compare_options = array_diff_key( $options, $args );   
 	    if ( empty( $options ) || !empty( $compare_options ) ) {
-	        update_option( $this->setting_api_name , $merged_options );
+	        update_option( $this->option_name , $merged_option );
 	    }
-	    return $merged_options;
+	    return $merged_option;
 	}
 
-	public static function uninstall() {	 
+	public function register_page($args = array()) {
+		
+		$default_page = array (
+			'page_title' => '',
+			'menu_title' => '',
+			'capability' => 'manage_options',
+			'menu_slug' => '',
+			
+		);
+
+		$merged_page = wp_parse_args( $args, $default_page );
+
+		add_action( 'admin_menu', function() use ($merged_page) {
+			extract($merged_page);
+
+		 	add_options_page( 
+				 $page_title , 
+				 $menu_title, 
+				 $capability, 
+				 $menu_slug, 
+				 function(){
+					$this->config_page_load();
+				 } );
+		} );
+	}
+
+	public function register_setting($args = array()) {
+
+		$default_setting = array(
+			'id'		=>	'',
+			'title'		=>	'',
+			'section'	=>	''
+		);
+
+		$merged_setting = wp_parse_args( $args, $default_setting );
+		$this->register_setting_vars = $merged_setting;
+
+		add_action( 'admin_init', function() use ($merged_setting){
+			$this->register_setting_init($merged_setting);
+		} );
+	}
+
+	public function register_field($args = array()) {
+
+		$default_field = array(
+			'id'	=>	'',
+			'name' 	=>	'',
+			'value' =>	'',
+			'type' 	=>	'textbox'
+		);
+
+		$merged_field = wp_parse_args( $args, $default_field );
+		extract( $merged_field );
+
+		if(empty($id) || empty($name))
+			return;
+
+		add_action( 'admin_init', function() use ($merged_field){
+			$this->register_field_init($merged_field);
+		} );
+	}
+
+	public function main_setting_section_callback() {
+		
+	}
+
+	public function validate_options() {
+
+	}
+
+	public function uninstall() {	 
 		// Check if options exist and delete them if present 
-		if ( get_option( $this->setting_api_name ) != false ) { 
-		    delete_option( $this->setting_api_name );
+		if ( get_option( $this->option_name ) != false ) { 
+		    delete_option( $this->option_name );
 		} 
 	}
+	#endregion
+
+	#region private
+	private function register_setting_init($args_setting) {
+
+		extract( $args_setting );
+
+		register_setting( 
+			$this->option_group,
+			$this->option_name,
+			array( $this, 'validate_options' )
+		);
+
+		add_settings_section( 
+			$id,
+			$title, 
+			array( $this, 'main_setting_section_callback' ),
+			$section );
+	}
+
+	private function register_field_init($args_field) {
+		extract( $args_field );
+		
+		$arr_setting = $this->register_setting_vars;
+
+		add_settings_field( 
+			$id, 
+			$name,
+			field_type($type), 
+			$arr_setting["section"],
+			$arr_setting["id"], 
+			$args_field 
+		);
+	}
+	#endregion
+
+	#region protected
+
+	protected function config_page_load() {
+		$arr_setting = $this->register_setting_vars;
+		?>
+
+		<div id="<?php echo $this->option_name ?>" class="wrap">
+
+			<form name="<?php echo $this->option_name ?>_form_settings_api" method="post" action="options.php">
+
+			<?php settings_fields( $arr_setting["id"] ); ?>
+			<?php do_settings_sections( $arr_setting["section"] ); ?> 
+
+			<input type="submit" value="Submit" class="button-primary" />
+			
+			</form>
+
+		</div>
+	<?php
+	}
+
+	protected function field_type($type_name): string {
+		if($type_name === 'textbox')
+			return 'html_display_text_field';
+		if($type_name === 'checkbox')
+			return 'html_display_check_box';
+		if($type_name === 'textarea')
+			return 'html_display_text_area';
+		else
+			return '';
+	}
+
+	protected function html_display_text_field( $data = array() ) {
+		extract( $data ); ?>
+		<input type="text" name="ch3sapi_options[<?php echo $name; ?>]" value="<?php echo $value; ?>"/><br />
+	<?php 
+	}
+
+	protected function html_display_check_box( $data = array() ) {
+		extract ( $data );
+		?>
+		<input type="checkbox" name="ch3sapi_options[<?php echo $name; ?>]" <?php if ( $value ) echo ' checked="checked" '; ?> />
+	<?php 
+	}
+
+	protected function html_display_text_area( $data = array() ) {
+		extract ( $data );
+		?>
+		<textarea type='text' name='ch3sapi_options[<?php echo $name; ?>]' rows='5' cols='30'><?php echo $value; ?></textarea>
+	<?php 
+	}
+	#endregion
+
 }
 
-// new TEK_Admin_Setting("");
+// return new TEK_Admin_Setting("tokitek_options");
--- tos.php
+++ tos.php
@@ -12,3 +12,22 @@ define( 'TOS_PLUGIN_DIR', plugin_dir_path(__FILE__) );
 define( 'TOS_CLASSES_DIR', TOS_PLUGIN_DIR . 'classes/' );
 
 require_once TOS_CLASSES_DIR . 'class-tek-admin-setting.php';
+$page = new TEK_Admin_Setting("tokitek_options");
+
+$page->register_page(
+  array(
+    'page_title' => 'hello worlds',
+    'menu_title' => 'tokitek',
+    'capability' => 'manage_options',
+    'menu_slug' => 'hello_page'
+  )
+);
+
+$page->register_setting(
+  array(
+    'id' => 'ch3sapi_main_section',
+    'title' => 'Main Settings',
+    'section' => 'ch3sapi_settings_section'
+  )
+);
+
